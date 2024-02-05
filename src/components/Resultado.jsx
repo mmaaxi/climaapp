fb64af9f91fe87c1f875637f68388a55fa9e7590
Maximiliano Reyes <maxyreyes7@gmail.com>
2024-02-05 15:23:36 -0600

    Añadiendo el Resto de la Información y Mostrando el Clima

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -2,10 +2,16 @@ import useClima from "../hooks/useClima"
 
 function Resultado() {
   const { resultado } = useClima()
-  const { name } = resultado
+  const { name, main } = resultado
+  const kelvin= 273.15
   return (
-    <div className="contenedor">
+    <div className="contenedor clima">
       <h2>El clima de {name} es: </h2>
+      <p>{parseInt(main.temp - kelvin)} <span>&#x2103;</span></p>
+      <div className="temp_min_max">
+        <p>Mín: {parseInt(main.temp_min - kelvin)} <span>&#x2103;</span></p>
+        <p>Máx: {parseInt(main.temp_max - kelvin)} <span>&#x2103;</span></p>
+      </div>
     </div>
   )
 }
