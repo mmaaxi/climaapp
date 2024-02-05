00cd660dff9370fa422a95d3b14be2930b692fe4
Maximiliano Reyes <maxyreyes7@gmail.com>
2024-02-05 13:35:37 -0600

    Creando el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <div className='contenedor'>
+      <form action="">
+        <div className='campo'>
+          <label htmlFor="ciudad">Ciudad</label>
+          <input type="text" id='ciudad' name='ciudad'/>
+        </div>
+
+        <div className='campo'>
+          <label htmlFor="pais">País</label>
+          <select name="pais" id="pais">
+            <option value="">-- Seleccione un País --</option>
+            <option value="US">Estados Unidos</option>
+            <option value="MX">México</option>
+            <option value="AR">Argentina</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">España</option>
+            <option value="PE">Perú</option>
+          </select>
+        </div>
+        <input type="submit" value='Consultar Clima'/>
+      </form>
+    </div>
   )
 }
