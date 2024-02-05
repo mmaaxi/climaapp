720b36cd0e194fcd4d1493f4a9cf997aaee27c24
Maximiliano Reyes <maxyreyes7@gmail.com>
2024-02-05 13:57:37 -0600

    Leyendo entradas de datos en el Formulario y Validación

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import useClima from "../hooks/useClima"
+import { useState } from "react"
 
 export const Formulario = () => {
+
+  const [ alerta, setAlerta ] = useState('')
+  const { busqueda, datosBusqueda } = useClima()
+  const { ciudad, pais } = busqueda
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    if(Object.values(busqueda).includes('')){
+      setAlerta('Todos los campos son obligatorios')
+      return
+    }
+  }
   return (
     <div className='contenedor'>
-      <form action="">
+      {alerta && <p>{alerta}</p>}
+      <form onSubmit={handleSubmit}>
         <div className='campo'>
           <label htmlFor="ciudad">Ciudad</label>
-          <input type="text" id='ciudad' name='ciudad'/>
+          <input type="text" id='ciudad' name='ciudad' onChange={datosBusqueda} value={ciudad}/>
         </div>
 
         <div className='campo'>
           <label htmlFor="pais">País</label>
-          <select name="pais" id="pais">
+          <select name="pais" id="pais" onChange={datosBusqueda} value={pais}>
             <option value="">-- Seleccione un País --</option>
             <option value="US">Estados Unidos</option>
             <option value="MX">México</option>
